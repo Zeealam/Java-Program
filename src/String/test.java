d21779615493db13692aac4f01dcf2e9b0be8603
Zeealam <zeeshan.alam7@gmail.com>
2024-10-31 14:57:30 +0530

    update main method

--- src/String/test.java
+++ src/String/test.java
@@ -4,6 +4,11 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
 
 	}
 
